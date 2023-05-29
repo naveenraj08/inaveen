a511401ad1ccef86a3674694afcd900ae0750350
Naveen raj.K <47858683+Naveen-Raj-08@users.noreply.github.com>
2023-05-29 22:22:22 +0530

    Configured the font gloablly

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,17 @@ module.exports = {
         about: "#374579",
         facebook: "#3b5998",
         linkedin: "#0A66C2",
-        instagram: "var(--insta-gradient)"
-      }
+        instagram: "var(--insta-gradient)",
+      },
     },
     fontFamily: {
-      Rubik: {
-        Regular: ['rubik-regular', 'sans-serif'],
-        Medium: ['rubik-medium', 'sans-serif'],
-        SemiBold: ['rubik-semi-bold', 'sans-serif'],
-        Bold: ['rubik-bold', 'sans-serif'],
-        ExtraBold: ['rubik-extra-bold', 'sans-serif'],
-        Black: ['rubik-black', 'sans-serif'],
-      }
-    }
+      regular: "Rubik-regular",
+      medium: "Rubik-medium",
+      semibold: "Rubik-semibold",
+      bold: "Rubik-bold",
+      extrabold: "Rubik-extrabold",
+      black: "Rubik-black",
+    },
   },
   plugins: [],
-}
+};
